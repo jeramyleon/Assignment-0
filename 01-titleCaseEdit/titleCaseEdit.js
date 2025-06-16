ee98e226842f05a43f847b9672410c48e199506c
jeramyleon <jeramyleon@gmail.com>
2025-06-16 02:29:00 -0400

    completed 01-titleCaseEdit

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,16 @@
 function titleCaseEdit(title) {
   // Insert code here;
+  words = title.split(' '); 
+  capitalizedWords = words.map(word => {
+    if (word.length === 0) return ''; 
+
+    firstLetter = word.charAt(0).toUpperCase();
+    restOfWord = word.slice(1).toLowerCase();
+
+    return firstLetter + restOfWord; 
+  });
+
+  return capitalizedWords.join(' ');
 }
 
 // Do not edit this line;
