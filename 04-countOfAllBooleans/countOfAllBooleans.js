ad33d44cbebff628985d81927d3bdb096451d470
jeramyleon <jeramyleon@gmail.com>
2025-06-25 19:53:23 -0400

    finished 5 problems so far

--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,5 +1,15 @@
 function countOfAllBooleans(arr) {
   // Insert code here;
+  let booleanCount = 0; 
+  // loop through array 
+  // if element is boolean, add 1 to booleanCount 
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === 'boolean') {
+      booleanCount++; 
+    }
+  }
+
+  return booleanCount; 
 }
 
 // Do not edit this line;
--- 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
+++ 05-countOfAllBooleansAndStrings/countOfAllBooleansAndStrings.js
@@ -1,5 +1,20 @@
 function countOfAllBooleansAndStrings(arr) {
   // Insert code here;
+  let booleanAndStringCount = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+
+    if (typeof arr[i] === 'boolean') {
+      booleanAndStringCount++; 
+    }
+
+    if (typeof arr[i] === 'string') {
+      booleanAndStringCount++;
+    }
+
+  }
+  
+  return booleanAndStringCount;
 }
 
 // Do not edit this line;
