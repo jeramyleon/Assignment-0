82479b5d72150f4d1e896ef677df2828b5e65095
jeramyleon <jeramyleon@gmail.com>
2025-06-26 00:03:22 -0400

    done with all

--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,5 +1,14 @@
 function setUnionOfAnyAmountOfSets(...args) {
   // Insert code here;
+  const mergedSet = new Set();
+
+  for (let i = 0; i < args.length; i++) {
+    args[i].forEach(num => {
+      mergedSet.add(num);
+    });
+  }
+
+  return mergedSet;
 }
 
 // Do not edit this line;
