a93ff93383c34b08f8219821e0078d480c820384
jeramyleon <jeramyleon@gmail.com>
2025-06-26 00:01:12 -0400

    finished problems 16 to 19, done with them all

--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -5,7 +5,24 @@ class MySolution {
 
   binarySearch(nums, target) {
     // Insert code here;
+    if (nums.length === 0) {
+      return false;
+    }
+
+    const middleIndex = Math.floor(nums.length / 2);
+    const middleValue = nums[middleIndex];
+
+    if (middleValue === target) {
+      return true; 
+    }
+
+    if (target < middleValue) {
+      return this.binarySearch(nums.slice(0, middleIndex), target);
+    } else if (target > middleValue) {
+      return this.binarySearch(nums.slice(middleIndex + 1), target);
+    }
   }
+
 }
 
 // Do not edit this line;
